15de627dc0f021dfdc5f9722d613b6abe51c7b0c
Mitesh2001 <miteshladva.dgrs@gmail.com>
2025-04-22 23:16:48 +0530

    Refactor MenuItemts component to use SectionList for improved rendering of menu items

--- app/components/MenuItemts.tsx
+++ app/components/MenuItemts.tsx
@@ -1,46 +1,70 @@
 import React from 'react';
-import { FlatList, SectionList, StyleSheet, Text, View } from 'react-native';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
 const menuItemsToDisplay = [
-    { name: 'Hummus', price: '$5.00', id: '1A' },
-    { name: 'Moutabal', price: '$5.00', id: '2B' },
-    { name: 'Falafel', price: '$7.50', id: '3C' },
-    { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-    { name: 'Kofta', price: '$5.00', id: '5E' },
-    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-    { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-    { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-    { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-    { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-    { name: 'Fries', price: '$3.00', id: '11K' },
-    { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-    { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-    { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-    { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-    { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-    { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-    { name: 'Baklava', price: '$3.00', id: '18S' },
-    { name: 'Tartufo', price: '$3.00', id: '19T' },
-    { name: 'Tiramisu', price: '$5.00', id: '20U' },
-    { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+    {
+        title: 'Appetizers',
+        data: [
+            { name: 'Hummus', price: '$5.00' },
+            { name: 'Moutabal', price: '$5.00' },
+            { name: 'Falafel', price: '$7.50' },
+            { name: 'Marinated Olives', price: '$5.00' },
+            { name: 'Kofta', price: '$5.00' },
+            { name: 'Eggplant Salad', price: '$8.50' },
+        ],
+    },
+    {
+        title: 'Main Dishes',
+        data: [
+            { name: 'Lentil Burger', price: '$10.00' },
+            { name: 'Smoked Salmon', price: '$14.00' },
+            { name: 'Kofta Burger', price: '$11.00' },
+            { name: 'Turkish Kebab', price: '$15.50' },
+        ],
+    },
+    {
+        title: 'Sides',
+        data: [
+            { name: 'Fries', price: '$3.00', id: '11K' },
+            { name: 'Buttered Rice', price: '$3.00' },
+            { name: 'Bread Sticks', price: '$3.00' },
+            { name: 'Pita Pocket', price: '$3.00' },
+            { name: 'Lentil Soup', price: '$3.75' },
+            { name: 'Greek Salad', price: '$6.00' },
+            { name: 'Rice Pilaf', price: '$4.00' },
+        ],
+    },
+    {
+        title: 'Desserts',
+        data: [
+            { name: 'Baklava', price: '$3.00' },
+            { name: 'Tartufo', price: '$3.00' },
+            { name: 'Tiramisu', price: '$5.00' },
+            { name: 'Panna Cotta', price: '$5.00' },
+        ],
+    },
 ];
 
 const MenuItemts = () => {
     return (
         <View style={menuStyles.container}>
-            <FlatList
-                data={menuItemsToDisplay}
+            <SectionList
+                sections={menuItemsToDisplay}
                 renderItem={({ item }) => (
-                    <View style={menuStyles.itemContainer} >
-                        <Text style={menuStyles.itemText}>
-                            {item.name}
-                        </Text>
-                        <Text style={menuStyles.itemText}>
-                            {item.price}
-                        </Text>
+                    <View style={menuStyles.itemContainer}>
+                        <Text style={menuStyles.itemText}>{item.name}</Text>
+                        <Text style={menuStyles.itemText}>{item.price}</Text>
                     </View>
                 )}
-                style={{ flex: 1, padding: 20 }}
+                renderSectionHeader={({ section }) => {
+                    return (
+                        <Text style={menuStyles.sectionHeader}>
+                            {section.title}
+                        </Text>
+                    )
+                }}
+                stickySectionHeadersEnabled
+                style={{ flex: 1 }}
             />
         </View>
     );
@@ -61,5 +85,12 @@ const menuStyles = StyleSheet.create({
     itemText: {
         fontSize: 20,
         color: "#F4CE14"
+    },
+    sectionHeader: {
+        fontSize: 26,
+        backgroundColor: "#F4CE14",
+        color: "black",
+        textAlign: "center",
+        flexWrap: "wrap"
     }
 });
