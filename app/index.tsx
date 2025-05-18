d80c02ab3c42bde4979c82f112f68e108764f0a2
Mitesh2001 <miteshladva.dgrs@gmail.com>
2025-05-18 12:17:18 +0530

    react navigation stack and react navigation native

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,31 @@
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import LittleLemonHeader from './components/LittleLemonHeader';
+import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './components/WelcomeScreen';
-import MenuItemts from './components/MenuItemts';
+import LittleLemonHeader from './components/LittleLemonHeader';
 import LoginScreen from './components/LoginScreen';
+import WelcomeScreen from './components/WelcomeScreen';
+const Stack = createStackNavigator();
 export default function Index() {
+
   return (
-    <>
-      <View
-        style={styles.container}>
-        <LittleLemonHeader />
-        <WelcomeScreen />
-        <LittleLemonFooter />
-      </View>
-    </>
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <View
+          style={styles.container}>
+          <LittleLemonHeader />
+          <Stack.Navigator initialRouteName="welcome">
+            <Stack.Screen name='login' component={LoginScreen} />
+            <Stack.Screen name='welcome' component={WelcomeScreen} />
+          </Stack.Navigator>
+          <View style={styles.footerContainer}>
+            <LittleLemonFooter />
+          </View>
+        </View>
+      </NavigationContainer>
+    </NavigationIndependentTree>
   );
 }
 
@@ -23,5 +33,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#333333',
-  }
-})
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
